0f0c3b9399ed0f75c678b2e2e39d9360c2c46496
Jim Bradley <derivativedude123@gmail.com>
2021-07-16 01:51:02 -0500

    begin standarddizing Doctor pydantic class

---
+++ src/doctor_class.py
@@ -0,0 +1,87 @@
+import pandas
+
+from pydantic import (
+    BaseModel, Field, validator, ValidationError
+)
+
+from typing import (
+    List, Dict, Optional, Type
+)
+    
+class Address(BaseModel):
+    line1: Optional[str] = None
+    line2: Optional[str] = None
+    city:  Optional[str] = None
+    state: Optional[str] = None
+    zip_:  Optional[str] = Field(None, alias='zip')
+
+class DoctorInfo(BaseModel):
+    phones:    List[str] = []
+    websites:  List[str] = []
+    languages: List[str] = []
+    misc:      Dict[str, str] = {}
+
+    class Config:
+        anystr_strip_whitespace = True
+
+class Title(BaseModel):
+    prefix:      str
+    first_name:  str
+    middle_name: Optional[List[str]] = []
+    last_name:   List[str]
+    degrees:     List[str] = []
+
+    _VALID_PREFIXES = ['Dr.', 'Prof.', 'Mrs.', 'Mr.', 'Ms.']
+
+    @validator('prefix')
+    def assert_prefix_is_valid(cls, prefix_str: str) -> str:
+        if prefix_str not in cls._VALID_PREFIXES:
+            raise ValidationError('must be one of Title._VALID_PREFIXES')
+        return prefix_str
+
+class Doctor(BaseModel):
+    title:                  Title
+    address:                Optional[Address] = None
+    info:                   Optional[DoctorInfo] = None
+    areas_of_concentration: List[str] = []
+    board_cert:             Optional[str] = None
+
+    def to_DataFrame(self) -> pandas.DataFrame:
+        fields = recursively_get_all_fields(self.__class__)
+        df = pandas.DataFrame.from_dict(flatten(self.dict()), orient='index').transpose()
+        return df
+
+
+def recursively_get_all_fields(model_cls: Type[BaseModel]) -> List[str]:
+    fields = []
+
+    def _recurse(model_cls: Type[BaseModel]) -> None:
+        for info in model_cls.__fields__.values():
+            if issubclass(info.type_, BaseModel):
+                _recurse(info.type_)
+            else:
+                fields.append(info.name)
+    _recurse(model_cls)
+    return fields
+
+def flatten(d: Dict) -> Dict[str, str]:
+    out = {}
+
+    def _recurse(d: Dict) -> None:
+        for k, v in d.items():
+            if isinstance(v, dict) and k != 'misc':
+                _recurse(v)
+            else:
+                out[k] = v
+
+    _recurse(d)
+    return out
+
+if __name__ == '__main__':
+    doc = Doctor(
+        title = Title(prefix='Dr.', first_name='Liz', last_name=['Bradley'])
+    )
+    print(flatten(doc.dict()))
+    print(
+        doc.to_DataFrame()
+    )
--- src/endocrinologists.py
+++ src/endocrinologists.py
@@ -1,110 +1,69 @@
-import requests
-import pandas
 import re
-from pydantic import BaseModel, Field, validator, ValidationError
+from typing import Dict, List, Optional, Type
+
+import pandas
+import requests
 from bs4 import BeautifulSoup
+from pydantic import (BaseModel, Field, ValidationError, root_validator,
+                      validator)
 
-from typing import (
-    Dict, List, Optional, Type
-)
-
-class Title(BaseModel):
-    prefix:      str
-    first_name:  str
-    middle_name: Optional[List[str]] = []
-    last_name:   List[str]
-    degrees:     List[str]
+from doctor_class import (Doctor, DoctorInfo, Title, flatten,
+                          recursively_get_all_fields)
 
 
-class Address(BaseModel):
-    line1: Optional[str] = None
-    line2: Optional[str] = None
-    city: Optional[str]  = None
-    state: Optional[str] = None
-    zip_: Optional[str]  = Field(None, alias='zip')
-
-class DoctorInfo(BaseModel):
-    phone:                  Optional[str] = None
-    fax:                    Optional[str] = None
-    website:                Optional[str] = None
-    languages:              List[str] = []
-    misc:                   Dict[str, str] = {}
-
+class EndocrinologistInfo(DoctorInfo):
     @validator('languages', pre=True)
     def lang_val(cls, lang_str):
-        return lang_str.strip().replace(', ', ',').split(',')
-
-    @validator('phone')
-    def phone_val(cls, phone_str):
-        return phone_str.strip()
-
-    @validator('fax')
-    def fax_val(cls, fax_str):
-        return fax_str.strip()
+        if isinstance(lang_str, list):
+            return lang_str
+        return lang_str.replace(', ', ',').split(',')
 
 
-class Endocrinologist(BaseModel):
-    title: Title
-
-    # TODO
-    #job:                    str
-    #place:                  str
-    #address:                Address
-
-    info:                   Optional[DoctorInfo] = None
-    areas_of_concentration: List[str] = []
-    board_cert:             Optional[str] = None
-    
+class Endocrinologist(Doctor):
+    info: Optional[EndocrinologistInfo] = None
+        
 
 
-
-URL = 'https://www.hormone.org/find-an-endocrinologist/find-an-endocrinologist-results?specialty=Thyroid&country=UNITED+STATES&page=0'
-
-# TODO: Move from Impl test class to pydantic class
 class _EndocrinologistImpl:
+    URL = 'https://www.hormone.org/find-an-endocrinologist/find-an-endocrinologist-results?specialty=Thyroid&country=UNITED+STATES&page=0'
     # https://regex101.com/r/gZVDIk/1
     TITLE_REGEX = re.compile(
         r'^(.+\.) *([A-Za-z]+?) +(?:((?:(?:[A-Z] )+[A-Z](?= ))|(?:[A-Za-z\-]+)) )?([A-Za-z\- ]+) *(?:, ?([A-Za-z\-,\. ]+))?$'
     )
 
-
     def __init__(self, soup: BeautifulSoup):
         self.d = {}
-        r = requests.get(URL)
 
         self.soup = soup
 
         for field, info in Endocrinologist.__fields__.items():
             try:
                 func = getattr(self, f'_get_{field}')
-                self.d[field] = func()
+                res = func()
+                self.d[field] = res
             except Exception as e:
-                # print(field, e)
-                if info.required: 
+                #print(field, e)
+                if info.required:
                     pass
-                    #raise e
-
-    def to_endocrinologist(self):
-            return Endocrinologist(**self.d)
-        
-
+                    # raise e
 
 
     def _get_title(self) -> Title:
-        raw_text = self.soup.find(class_='endocrinologist-list-item__title').get_text()
+        raw_text = self.soup.find(
+            class_='endocrinologist-list-item__title').get_text()
         fields = self.TITLE_REGEX.search(raw_text).groups()
         prefix, first, middle, last, degrees = fields
 
-        middle  = middle.strip().split(' ') if middle else [] # TODO: make validators
-        last    = last.strip().split(' ') if last else []
+        middle = middle.strip().split(' ') if middle else []  # TODO: make validators
+        last = last.strip().split(' ') if last else []
         degrees = degrees.replace(' ', '').split(',') if degrees else []
 
         return Title(**{
-            'prefix'     : prefix,
-            'first_name' : first,
+            'prefix': prefix,
+            'first_name': first,
             'middle_name': middle,
-            'last_name'  : last,
-            'degrees'    : degrees
+            'last_name': last,
+            'degrees': degrees
         })
 
     def _get_info(self) -> DoctorInfo:
@@ -116,7 +75,6 @@ class _EndocrinologistImpl:
             v = elem.get_text().strip(k)
             k = k.strip(':').lower()
 
-            # TODO: instead of lower() use pydantic aliases
             if k in DoctorInfo.__fields__ and k != 'misc':
                 d[k] = v
             else:
@@ -128,54 +86,40 @@ class _EndocrinologistImpl:
         concentrations_elem = None
         for elem in self.soup.find_all(class_='endocrinologist-list-item__area-title'):
             if elem.get_text() == 'Area of Concentration':
-                concentrations_elem = elem.parent.find(class_='endocrinologist-list-item__description')
+                concentrations_elem = elem.parent.find(
+                    class_='endocrinologist-list-item__description')
                 break
 
-        concentrations = [i for i in concentrations_elem.children if str(i) != '<br/>']
+        concentrations = [
+            i for i in concentrations_elem.children if str(i) != '<br/>']
 
         return concentrations
 
     def _get_board_cert(self) -> str:
         for elem in self.soup.find_all(class_='endocrinologist-list-item__area-title'):
             if elem.get_text() == 'General Board Certification':
-                cert = elem.parent.find(class_='endocrinologist-list-item__area-description').get_text()
+                cert = elem.parent.find(
+                    class_='endocrinologist-list-item__area-description').get_text()
                 return cert
-        
 
-def recursively_get_all_fields(model: Type[BaseModel]) -> List[str]:
-    fields = []
-    def _recurse(model: Type[BaseModel]) -> None:
-        for info in model.__fields__.values():
-            if issubclass(info.type_, BaseModel):
-                _recurse(info.type_)
-            else:
-                fields.append(info.name)
-    _recurse(model)
-    return fields
-
-def flatten(d: Dict) -> Dict[str, str]:
-    out = {}
-    def _recurse(d: Dict) -> None:
-        for k, v in d.items():
-            if isinstance(v, dict) and k != 'misc':
-                _recurse(v)
-            else:
-                out[k] = v
-
-    _recurse(d)
-    return out
 
 if __name__ == '__main__':
+    
     df = pandas.DataFrame(columns=recursively_get_all_fields(Endocrinologist))
-    print(df.columns)
-    soup = BeautifulSoup(requests.get(URL).text, features='lxml')
-    for elem in soup.find_all(class_='endocrinologist-list-item'):
+    print(df)
+    r = requests.get(_EndocrinologistImpl.URL)
+    base_soup = BeautifulSoup(r.text, features='lxml')
+    for soup in base_soup.find_all(class_='endocrinologist-list-item'):
         try:
-            ei = _EndocrinologistImpl(elem).to_endocrinologist()
-            print(flatten(ei.dict()))
-            df = df.append(flatten(ei.dict()), ignore_index=True)
-            df.to_csv('data/endocrinologists.csv')
-        except Exception as e:
-            print(e) # FIXME
-        
-        
+            d = _EndocrinologistImpl(soup).d
+            end = Endocrinologist(**d)
+        except ValidationError:
+            print('ERROR')
+            continue
+        df = df.append(end.to_DataFrame(), ignore_index=True)
+        print(len(df))
+
+    df.to_csv('data/endocrinologists.csv')
+    
+
+
