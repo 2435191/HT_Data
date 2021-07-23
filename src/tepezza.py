6a20d75cd8c459c8227d8cc8fe5387319e7f25c5
Jim Bradley <derivativedude123@gmail.com>
2021-07-23 18:06:19 -0500

    update tepezza

--- src/tepezza.py
+++ src/tepezza.py
@@ -1,5 +1,4 @@
-# 88255
-
+import logging
 import json
 import os
 import re
@@ -20,13 +19,10 @@ import dpkt
 import geopandas as gpd
 import pandas
 from matplotlib import pyplot as plt
-from pydantic import Field, root_validator, validator
 from shapely.ops import nearest_points
 
-from doctor_class import Address, Doctor, DoctorInfo, Title
 
-
-class Colors:  # TODO: use actual package
+class Colors:  # TODO: use actual package or logging
     OK = '\033[94m'
     YELLOW = '\033[93m'
     FAIL = '\033[91m'
@@ -34,74 +30,7 @@ class Colors:  # TODO: use actual package
     UNDERLINE = '\033[4m'
 
 
-
-
-class TepezzaTitle(Title):
-    @root_validator(pre=True)
-    def init(cls, values):
-        print(values)
-        out = values.copy()
-        for k, v in values.items():
-            out[k.lower()] = v
-            del out[k]
-
-        for field_name in ['middle_name', 'last_name']:
-            out[field_name] = [out[field_name]] if out[field_name] else []
-        return out
-
-
-class TepezzaAddress(Address):
-    line1: Optional[str] = Field(None, alias='address_line1')
-    line2: Optional[str] = Field(None, alias='address_line2')
-
-    @root_validator(pre=True)
-    def init(cls, values):
-        out = values.copy()
-        for k, v in values.items():
-            out[k.lower()] = v
-            del out[k]
-        return out
-
-
-class TepezzaDoctorInfo(DoctorInfo):
-    @root_validator(pre=True)
-    def init(cls, values):
-        out = {}
-        out['phones'] = [values['PHONE'], values['MOBILE']]
-
-        del values['PHONE']
-        del values['MOBILE']
-        out['misc'] = values
-
-        return out
-
-
-class TepezzaDoctor(Doctor):
-    source:                 str = 'tepezza'
-    title:                  TepezzaTitle
-    info:                   Optional[TepezzaDoctorInfo] = None
-    address:                Optional[TepezzaAddress] = None
-    areas_of_concentration: List[str] = []
-    board_cert:             Optional[str] = None
-
-    @root_validator(pre=True)
-    def init(cls, values):
-        d = {'areas_of_concentration': [values['AMA_SPECIALITY']]}
-        title_fields   = ['FIRST_NAME', 'MIDDLE_NAME', 'LAST_NAME']
-        address_fields = ['ADDRESS_LINE1', 'ADDRESS_LINE2', 'CITY', 'STATE', 'ZIP']
-        d['title'] = {
-            k: v for k, v in values.items() if k in title_fields
-        }
-        d['address'] = {
-            k: v for k, v in values.items() if k in address_fields
-        }
-        d['info'] = {
-            k: v for k, v in values.items() if k not in address_fields + title_fields
-        }
-        return d
-
-
-class TepezzaInterface:
+class TepezzaApi:
     """Partially automate data return from tepezza.com.
     This interface allows a user to efficiently scrape data,
     even though the website uses reCaptcha. It does this by
@@ -109,14 +38,15 @@ class TepezzaInterface:
     looks up zipcodes that an iterative algorithm generates (so
     as to cover the United States efficiently).
 
-    :return: A `TepezzaInterface` instance. Call the `startup` method for functionality.
-    :rtype: TepezzaInterface
+    :return: A `TepezzaApi` instance. Call the `logger` method for functionality.
+    :rtype: TepezzaApi
     """
     TSHARK = '/Applications/Wireshark.app/Contents/MacOS/tshark'
     PATH_TO_CHROME = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
     TMP_CHROME_PROFILE = '/Users/eab06/Desktop/WJB/PythonProjects/HT_Data/src/__TMP_CHROME_PROFILE'
     TMP_CHROME_SSL_LOG = '/Users/eab06/Desktop/WJB/PythonProjects/HT_Data/src/__TMP_SSL_KEY.log'
-    SHAPEFILES = '/Users/eab06/Desktop/WJB/PythonProjects/HT_Data/src/tepezza_shapefiles'
+    TMP_GATHERED_DATA_LOG = '/Users/eab06/Desktop/WJB/PythonProjects/HT_Data/__TMP_DATA_GATHERED.json'
+    SHAPEFILES = '/Users/eab06/Desktop/WJB/PythonProjects/HT_Data/data/tepezza_shapefiles'
 
     CRS = "+proj=aeqd +lat_0=90 +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m no_defs"  # ESRI:102016
     TSHARK_CMD = f'{TSHARK} -l -x -i en0 -o ssl.keylog_file:{TMP_CHROME_SSL_LOG} -Y json -T ek'
@@ -128,11 +58,31 @@ class TepezzaInterface:
     def startup(self) -> None:
         """Load the relevant shapefiles and start the Chrome and tshark subprocesses.
         """
-        self._load_shps()
-        self._load_chrome()
+        self.startup_logger = logging.getLogger("startup")
+        self._load_shps(self.startup_logger)
+        self._load_chrome(self.startup_logger)
+        
+        while True:
+            in_ = input(
+                f"Is Chrome loaded? Ready to begin? Press {Colors.YELLOW}(y){Colors.ENDC}. ")
+            if in_ == "y":
+                break
 
-    def _load_shps(self) -> None:
-        print("Reading zipcode shapefile...")  # TODO: use logging
+        self.network_subp = subprocess.Popen(
+            shlex.split(self.TSHARK_CMD), stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=1, universal_newlines=True)
+        os.set_blocking(self.network_subp.stdout.fileno(), False)
+        os.set_blocking(self.network_subp.stderr.fileno(), False)
+
+        
+        # proceeding too quickly will break things. Added a busy wait from self.network_subp.stderr to fix this
+        while self.network_subp.stderr.readline() == '':
+            pass
+        
+        self.startup_logger.info(f"{Colors.OK}Initialization complete; proceed to Tepezza.{Colors.ENDC}")
+        
+
+    def _load_shps(self, logger: logging.Logger) -> None:
+        logger.info("Reading zipcode shapefile...")  # TODO: use logging
 
         # shapefile from
         # https://www2.census.gov/geo/tiger/TIGER2019/ZCTA5/tl_2019_us_zcta510.zip,
@@ -140,18 +90,17 @@ class TepezzaInterface:
         self._shp = gpd.read_file(os.path.join(self.SHAPEFILES, 'zipcodes'))
         self._shp.set_index('ZCTA5CE10', inplace=True)
 
-        print("Reading dissolved shapefile...")
+        logger.info("Reading dissolved shapefile...")
         self._incomplete = gpd.read_file(
             os.path.join(self.SHAPEFILES, 'dissolved/'))
         self._incomplete.to_crs(crs=self.CRS, inplace=True)  # FIXME
 
-        print("Reading representative multipoint shapefile...")
+        logger.info("Reading representative multipoint shapefile...")
         self._repr_multipoint = gpd.read_file(
             os.path.join(self.SHAPEFILES, 'representative_pts_multipoint/'))\
             .geometry[0]
 
-    def _load_chrome(self) -> None:
-        print(self.TMP_CHROME_PROFILE, self.TMP_CHROME_SSL_LOG)
+    def _load_chrome(self, logger: logging.Logger) -> None:
         """Open new Chrome window in a subprocess,
         so as to have decryption keys go into
         `TMP_CHROME_SSL_LOG`.
@@ -167,49 +116,48 @@ class TepezzaInterface:
              '--disable-logging',
              f"--user-data-dir={self.TMP_CHROME_PROFILE}"],
             stdout=subprocess.DEVNULL)
-        print(
+        logger.info(
             f"New Chrome profile launched; {Colors.FAIL}Do not proceed to Tepezza{Colors.ENDC}.")
-
-        print(f"{Colors.OK}Initialization complete.{Colors.ENDC}")
-
-        while True:
-            in_ = input(
-                f"Is Chrome loaded? Ready to begin? Press {Colors.YELLOW}\"y\"{Colors.ENDC}.")
-            if in_ == "y":
-                break
-
-        self.network_subp = subprocess.Popen(
-            shlex.split(self.TSHARK_CMD), stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, bufsize=1, universal_newlines=True)
-        os.set_blocking(self.network_subp.stdout.fileno(), False)
-        
-        # BUG: proceeding too quickly will break things. Add a busy wait from self.network_subp.stderr to fix this
-        print(f"{Colors.YELLOW}Proceed to Tepezza.{Colors.ENDC}")
 
     def __enter__(self):
         return self
 
     def __exit__(self, *_):
+        self.exit_logger = logging.getLogger("exit cleanup")
         try:
             self.chrome_subp.kill()
         except AttributeError:
-            pass
+            self.exit_logger.debug("No chrome subprocess found")
+        else:
+            self.exit_logger.debug("Chrome subprocess killed")
 
         try:
             self.network_subp.kill()
         except AttributeError:
-            pass
+            self.exit_logger.debug("No network subprocess found")
+        else:
+            self.exit_logger.debug("Network subprocess killed")
 
-        if os.path.exists(self.TMP_CHROME_PROFILE):
-            shutil.rmtree(self.TMP_CHROME_PROFILE)
-        if os.path.exists(self.TMP_CHROME_SSL_LOG):
-            os.remove(self.TMP_CHROME_SSL_LOG)
+        for path in [
+            self.TMP_CHROME_PROFILE,
+            self.TMP_GATHERED_DATA_LOG,
+            self.TMP_CHROME_SSL_LOG
+        ]:
+            if os.path.exists(path):
+                try:
+                    if os.path.isdir(path):
+                        shutil.rmtree(path)
+                    elif os.path.isfile(path):
+                        os.remove(path)
+                except PermissionError as e:
+                    self.exit_logger.warning(e, exc_info=True)
 
-        print(f"{Colors.OK}Cleanup complete.{Colors.ENDC}")
+        self.exit_logger.info(f"{Colors.OK}Cleanup complete.{Colors.ENDC}")
 
-    def _watch_network(self) -> 'JSON':
+    def _watch_network(self, logger: logging.Logger) -> 'JSON':
         while True:
             if input(
-                f"Once zipcode has been searched, press {Colors.YELLOW}\"y\"{Colors.ENDC} to continue. "
+                f"Once zipcode has been searched, press {Colors.YELLOW}(y){Colors.ENDC} to continue. "
             ) == 'y':
                 break
         counter = 0
@@ -224,7 +172,7 @@ class TepezzaInterface:
                 s = b''
 
             except (json.JSONDecodeError, json.decoder.JSONDecodeError) as e:  # overflowed
-                print(counter, str(e)[:min(1000, len(str(e)))], type(e))
+                logger.info(e, exc_info=True)
                 s += in_
                 try:
                     pkt = json.loads(s)
@@ -244,12 +192,13 @@ class TepezzaInterface:
                     i.pop('GeoLocation')
                 return d['data']
             except (KeyError, IndexError, AssertionError, TypeError) as e:
-                with open(f'error_{counter}.json', 'wb+') as f:
-                    f.write(in_)
-                print(counter, str(e)[:min(1000, len(str(e)))], type(e))
+                #with open(f'error_{counter}.json', 'wb+') as f:
+                    #f.write(in_)
+                logger.info(e, exc_info=True)
+
             # i in d['data'] will be str if no data, so pop raises
             except AttributeError:
-                print('empty')
+                logger.info("returning empty data")
                 return []
 
     def get_data(self,
@@ -273,24 +222,33 @@ class TepezzaInterface:
         :rtype: pandas.DataFrame
         """
 
-        df = TepezzaDoctor.to_empty_DataFrame()
+        df = pandas.DataFrame()
 
         target_zip_name = starting_zip
         initial_area = self._incomplete.area.iloc[0]
 
-        while not self._incomplete.is_empty.all():  # FIXME: end condition
-            print(
+        self.logger = logging.getLogger("get data")
+        self.watch_network_logger = logging.getLogger("watch network")
+        while True:
+            self.logger.info(
                 f"Look up zipcode {Colors.YELLOW}{Colors.UNDERLINE}{target_zip_name}{Colors.ENDC}.")
             clipboard.copy(target_zip_name)
 
-            data = self._watch_network()
-            model = [TepezzaDoctor(**d) for d in data]
+            while True:
+                data = self._watch_network(self.watch_network_logger)
+                with open(self.TMP_GATHERED_DATA_LOG, 'w+') as f:
+                    json.dump(data, f, indent=2)
+                inp = input(
+                    f"Are data accurate (correct zipcode entered)? If not sure, check "
+                    f"{Colors.YELLOW}{self.TMP_GATHERED_DATA_LOG}{Colors.ENDC} for most recent observation. "
+                    f"Otherwise, enter {Colors.YELLOW}(y){Colors.ENDC} to proceed. "
+                )
+                if inp == 'y':
+                    break
 
-            if model is not []:
-                for m in model:
-                    df = df.append(m.to_DataFrame())
-                df.to_csv(filepath)
-            print(f"Data received and saved.")
+            df = df.append(data)
+            df.to_csv(filepath)
+            
 
             target_zip_obj = self._shp.at[target_zip_name, 'geometry']
 
@@ -308,19 +266,23 @@ class TepezzaInterface:
             complete_gdf = gpd.GeoDataFrame(
                 {'geometry': complete}, index=[0], crs=self.CRS)
 
-            print("Completed GeoDataFrame found.")
+            self.logger.info("Completed GeoDataFrame found.")
 
             self._incomplete = gpd.overlay(
                 self._incomplete, complete_gdf, how='difference')
 
-            self._incomplete.plot()
-            plt.show()
+            #self._incomplete.plot()
+            #plt.show()
 
-            print("Overlay operation complete.")
+            self.logger.info("Overlay operation complete.")
+
+            if self._incomplete.is_empty.all():
+                self.logger.info("Loop done.")
+                break
 
             pct_done = round(
-                (1 - self._incomplete.area.iloc[0] / initial_area) * 100, 4)
-            print(f"{Colors.YELLOW}{pct_done}{Colors.ENDC}% done by area.\n")
+                (1 - self._incomplete.area.iloc[0] / initial_area) * 100, 8)
+            self.logger.info(f"{Colors.YELLOW}{pct_done}{Colors.ENDC}% done by area.\n")
 
             target_point = self._incomplete.representative_point().iloc[0]
             nearest_repr_pt = nearest_points(
@@ -330,7 +292,7 @@ class TepezzaInterface:
             zipcode_index_idx = self._shp.sindex.query(nearest_repr_pt)[0]
             target_zip_name = self._shp.index[zipcode_index_idx]
 
-            print(
+            self.logger.info(
                 f"New points generated; {Colors.OK}cycle complete{Colors.ENDC}.\n")
 
         return df
@@ -340,12 +302,12 @@ if __name__ == '__main__':
     def rfunc(radii: Iterable[float]) -> float:
         return median(radii)
 
+    logging.basicConfig(level=logging.INFO)
+
     try:
-        with TepezzaInterface() as ti:
-            ti.startup()
-            ti.get_data('60609', rfunc, 'data/tepezza.csv', 50)
+        with TepezzaApi() as api:
+            api.startup()
+            api.get_data('60609', rfunc, 'data/_tepezza_raw.csv', 50)
 
     except KeyboardInterrupt:
         pass
-    
-
