53eb8e30e0ac0b162a6553d21091476d99d00b38
Jim Bradley <derivativedude123@gmail.com>
2021-09-11 15:20:25 -0500

    npi number

--- src/get_npi/query_npi_database.py
+++ src/get_npi/query_npi_database.py
@@ -1,7 +1,10 @@
 from pydantic import BaseModel, validator
+from pydantic.error_wrappers import ValidationError
 import requests
+import logging
 import json
-from typing import Dict, Any, Optional
+from typing import Dict, Any, Optional, List, Iterator
+import pandas
 
 class DoctorQuery(BaseModel):
     # TODO: add taxonomy
@@ -10,7 +13,7 @@ class DoctorQuery(BaseModel):
     _VALID_NPI_TYPES = ('NPI-1', 'NPI-2')
 
     version: str = '2.1'
-    enumeration_type: str = 'NPI-1' # doctors vs. orgs
+    enumeration_type: str = 'NPI-1' # doctors (NPI-1) vs. orgs (NPI-2)
 
     first_name: str
     last_name: str
@@ -37,7 +40,7 @@ class DoctorQuery(BaseModel):
     _MAX_PAGE_SKIP = 1000
     _MAX_DATA_RETURN = _MAX_PAGE_SKIP + max(_PAGE_SIZE_RANGE)
 
-    def query_result_paged(self, page_size: int = 100, stop_after: int = 1200) -> Dict[str, Any]:
+    def query_result_paged(self, page_size: int = 100, stop_after: int = 1200) -> Iterator[Dict[str, Any]]:
         assert page_size in self._PAGE_SIZE_RANGE, \
             f'invalid page_size of {page_size}: must be in {self._PAGE_SIZE}'
         assert stop_after <= self._MAX_DATA_RETURN, \
@@ -55,16 +58,103 @@ class DoctorQuery(BaseModel):
             yield res
 
     
-    
-
-
-
-    
-
-
 if __name__ == '__main__':
-    dq = DoctorQuery(first_name='David', last_name='Bradley')
-    for i, val in enumerate(dq.query_result_paged()):
-        with open(f'test_{i}.json', 'w+') as f:
-            json.dump(val, f, indent=2)
+    
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    logging.basicConfig()
+    
+    DF_SOURCE = 'data/processed/all_with_npi.csv'
+    DF_DEST   = 'data/processed/all_with_npi.csv'
+    OVERWRITE = False
+
+    df: pandas.DataFrame = pandas.read_csv(DF_SOURCE, index_col=0)
+
+    if 'npi' not in df.columns:
+        df['npi'] = -1
+
+    # least restrictive to most restrictive query params.
+    # multiple elements in a tuple means they are queried together (or not at all)
+    DROP_ORDER = (
+        ('city',),
+        ('postal_code',), 
+        ('state',),
+        ('subspecialty_code',),
+        ('first_name', 'last_name')
+    )
+    START_IDX = 3 # "bottom" of search
+
+
+    def get_queries(row: pandas.Series, min_: int, max_: int) -> DoctorQuery:
+        param_names = sum(DROP_ORDER[min_ : max_], ())
+        logger.debug(f'param_names: {param_names}')
+
+        query_params = {k: v for k, v in row.to_dict().items() if k in param_names}
+        return DoctorQuery(**query_params)
+
+    for df_index, row in df.iterrows():
+        if not OVERWRITE and row['npi'] != -1:
+            continue
+
+        logger.debug(f"finding npi for:\n{row}")
+
+        idx = START_IDX
+        prev_indices = []
+
+        df.at[df_index, 'npi'] = None
+        df.to_csv(DF_DEST)
+
+        while True:
+            try:
+                if idx in prev_indices:
+                    logger.warning(f'no solution found for:\n{row} (ping ponging concern: idx = {idx}, prev_indices = {prev_indices})')
+                    break
+                if idx < 0 or idx >= len(DROP_ORDER):
+                    logger.warning(f'no solution found for:\n{row} (exceeded array bounds: idx = {idx})')
+                    break
+
+                prev_indices.append(idx)
+
+                count = 0
+                result = None
+
+                try:
+                    results = get_queries(row, idx, len(DROP_ORDER)).query_result_paged()
+                except ValidationError:
+                    count = 2
+                else:
+                    for result in results:
+                        count += result['result_count']
+                            
+
+                        if count > 1:
+                            break
+
+                if count == 0:
+                    idx += 1
+                    logger.debug(f'too restrictive, increasing floor to {DROP_ORDER[idx:]} (idx = {idx})')
+                elif count == 1:
+                    npi = result['results'][0]['number']
+                    logger.info(f'found {npi} for:\n{row}')
+                    df.at[df_index, 'npi'] = npi
+                    break
+                elif count > 1:
+                    idx -= 1
+                    logger.debug(f'not restrictive enough, decreasing floor to {DROP_ORDER[idx:]} (idx = {idx})')
+            except KeyError as e:
+                while True:
+                    pass
+            
+
+            
+            
+                
+
+            
+
+
+
+
+
+
 
