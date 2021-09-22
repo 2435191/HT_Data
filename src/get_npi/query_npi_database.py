ece36357480256a8f64f72a053d7f824ee3bec88
Jim Bradley <derivativedude123@gmail.com>
2021-09-22 17:24:25 -0500

    format python file

--- src/get_npi/query_npi_database.py
+++ src/get_npi/query_npi_database.py
@@ -6,6 +6,7 @@ import json
 from typing import Dict, Any, Optional, List, Iterator
 import pandas
 
+
 class DoctorQuery(BaseModel):
     # TODO: add taxonomy
     _URL = 'https://npiregistry.cms.hhs.gov/api'
@@ -13,18 +14,17 @@ class DoctorQuery(BaseModel):
     _VALID_NPI_TYPES = ('NPI-1', 'NPI-2')
 
     version: str = '2.1'
-    enumeration_type: str = 'NPI-1' # doctors (NPI-1) vs. orgs (NPI-2)
+    enumeration_type: str = 'NPI-1'  # doctors (NPI-1) vs. orgs (NPI-2)
 
     first_name: str
     last_name: str
-    
+
     city:  str = ''
     state: str = ''
     postal_code: str = ''
     country_code: str = 'US'
 
     specialty_code: Optional[str] = None
-
 
     @validator('version')
     def assert_version_valid(cls, v):
@@ -35,7 +35,6 @@ class DoctorQuery(BaseModel):
         assert v is None or v in cls._VALID_NPI_TYPES
         return '' if v is None else v
 
-
     _PAGE_SIZE_RANGE = range(10, 201)
     _MAX_PAGE_SKIP = 1000
     _MAX_DATA_RETURN = _MAX_PAGE_SKIP + max(_PAGE_SIZE_RANGE)
@@ -45,11 +44,11 @@ class DoctorQuery(BaseModel):
             f'invalid page_size of {page_size}: must be in {self._PAGE_SIZE}'
         assert stop_after <= self._MAX_DATA_RETURN, \
             f'invalid stop_after of {stop_after}: must be <= {self._MAX_DATA_RETURN}'
-        
+
         params = self.dict()
         for page_skip in range(0, stop_after+1, page_size):
             params['limit'] = page_size
-            params['skip']  = page_skip
+            params['skip'] = page_skip
 
             resp = requests.get(self._URL, params=params)
             res = json.loads(resp.text)
@@ -57,90 +56,94 @@ class DoctorQuery(BaseModel):
                 return
             yield res
 
-    
+
 if __name__ == '__main__':
-    
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.INFO)
-    logging.basicConfig()
-    
-    DF_SOURCE = 'data/processed/all_with_npi.csv'
-    DF_DEST   = 'data/processed/all_with_npi.csv'
-    OVERWRITE = False
+    try:
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.INFO)
+        logging.basicConfig()
 
-    df: pandas.DataFrame = pandas.read_csv(DF_SOURCE, index_col=0)
+        DF_SOURCE = 'data/processed/all.csv'
+        DF_DEST = 'data/processed/all_with_npi3.csv'
+        OVERWRITE = True
 
-    if 'npi' not in df.columns:
-        df['npi'] = -1
+        df: pandas.DataFrame = pandas.read_csv(DF_SOURCE, index_col=0)
 
-    # least restrictive to most restrictive query params.
-    # multiple elements in a tuple means they are queried together (or not at all)
-    DROP_ORDER = (
-        ('city',),
-        ('postal_code',), 
-        ('state',),
-        ('specialty_code',),
-        ('first_name', 'last_name')
-    )
-    START_IDX = 3 # "bottom" of search
+        if 'npi' not in df.columns:
+            df['npi'] = -1
 
+        # least restrictive to most restrictive query params.
+        # multiple elements in a tuple means they are queried together (or not at all)
+        DROP_ORDER = (
+            ('city',),
+            ('postal_code',),
+            ('state',),
+            ('specialty_code',),
+            ('first_name', 'last_name')
+        )
+        START_IDX = 3  # "bottom" of search
 
-    def get_queries(row: pandas.Series, min_: int, max_: int) -> DoctorQuery:
-        param_names = sum(DROP_ORDER[min_ : max_], ())
-        logger.debug(f'param_names: {param_names}')
+        def get_queries(row: pandas.Series, min_: int, max_: int) -> DoctorQuery:
+            param_names = sum(DROP_ORDER[min_: max_], ())
+            logger.debug(f'param_names: {param_names}')
 
-        query_params = {k: v for k, v in row.to_dict().items() if k in param_names}
-        return DoctorQuery(**query_params)
+            query_params = {k: v for k, v in row.to_dict().items()
+                            if k in param_names}
+            return DoctorQuery(**query_params)
 
-    for df_index, row in df.iterrows():
-        if not OVERWRITE and row['npi'] != -1:
-            continue
+        for df_index, row in df.iterrows():
+            if not OVERWRITE and row['npi'] != -1:
+                continue
 
-        logger.debug(f"finding npi for:\n{row}")
+            logger.debug(f"finding npi for:\n{row}")
 
-        idx = START_IDX
-        prev_indices = []
+            idx = START_IDX
+            prev_indices = []
 
-        df.at[df_index, 'npi'] = None
-        df.to_csv(DF_DEST)
+            df.at[df_index, 'npi'] = None
+            df.to_csv(DF_DEST)
 
-        while True:
-            try:
-                if idx in prev_indices:
-                    logger.warning(f'no solution found for:\n{row} (ping ponging concern: idx = {idx}, prev_indices = {prev_indices})')
-                    break
-                if idx < 0 or idx >= len(DROP_ORDER):
-                    logger.warning(f'no solution found for:\n{row} (exceeded array bounds: idx = {idx})')
-                    break
-
-                prev_indices.append(idx)
-
-                count = 0
-                result = None
-
+            while True:
                 try:
-                    results = get_queries(row, idx, len(DROP_ORDER)).query_result_paged()
-                except ValidationError:
-                    count = 2
-                else:
-                    for result in results:
-                        count += result['result_count']
-                            
+                    if idx in prev_indices:
+                        logger.warning(
+                            f'no solution found for:\n{row} (ping ponging concern: idx = {idx}, prev_indices = {prev_indices})')
+                        break
+                    if idx < 0 or idx >= len(DROP_ORDER):
+                        logger.warning(
+                            f'no solution found for:\n{row} (exceeded array bounds: idx = {idx})')
+                        break
 
-                        if count > 1:
-                            break
+                    prev_indices.append(idx)
 
-                if count == 0:
-                    idx += 1
-                    logger.debug(f'too restrictive, increasing floor to {DROP_ORDER[idx:]} (idx = {idx})')
-                elif count == 1:
-                    npi = result['results'][0]['number']
-                    logger.info(f'found {npi} for:\n{row}')
-                    df.at[df_index, 'npi'] = npi
-                    break
-                elif count > 1:
-                    idx -= 1
-                    logger.debug(f'not restrictive enough, decreasing floor to {DROP_ORDER[idx:]} (idx = {idx})')
-            except KeyError as e:
-                while True:
-                    pass
+                    count = 0
+                    result = None
+
+                    try:
+                        results = get_queries(row, idx, len(
+                            DROP_ORDER)).query_result_paged()
+                    except ValidationError:
+                        count = 2
+                    else:
+                        for result in results:
+                            count += result['result_count']
+                            if count > 1:
+                                break
+
+                    if count == 0:
+                        idx += 1
+                        logger.debug(
+                            f'too restrictive, increasing floor to {DROP_ORDER[idx:]} (idx = {idx})')
+                    elif count == 1:
+                        npi = result['results'][0]['number']
+                        logger.info(f'found {npi} for:\n{row}')
+                        df.at[df_index, 'npi'] = npi
+                        break
+                    elif count > 1:
+                        idx -= 1
+                        logger.debug(
+                            f'not restrictive enough, decreasing floor to {DROP_ORDER[idx:]} (idx = {idx})')
+                except KeyError as e:
+                    logger.critical('exception:', exc_info=e)
+    except:
+        pass
